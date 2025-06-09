dce7b904aba8f4c19e3123a01aaf3e9c33f18701
Arka Ghosh <135110396+arkaa01@users.noreply.github.com>
2025-06-09 09:41:55 +0530

    Add files via upload

---
+++ emp_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from emp_app.models import Employee
+from .models import Role, Department
+
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Department)
